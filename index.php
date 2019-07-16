b477858f478ab5e853ba8988a4c67328922a6558
mahmoudmosaad50@gmail.com <mahmoudmosaad50@gmail.com>
2019-07-16 14:11:57 +0200

    initial code

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 require './controller/UsersController.php';
 require './controller/CategoryController.php';
 
-if(!isset($_GET['controller']))
-{
-    echo "Error 404 page not found"; die();
-}
-else
-{
-    $controller = new $_GET['controller'];
-    call_user_func(array($controller, $_GET['method']));
+if (isset($_GET['controller'])
+    && isset($_GET['method'])
+    && method_exists($_GET['controller'],$_GET['method'])
+    && is_callable($_GET['controller'],$_GET['method'])){
+
+    $controller = new $_GET['controller']();
+    call_user_func(array($controller,$_GET['method']));
+}else{
+    header('location: view/404.php');
+    exit;
 }
